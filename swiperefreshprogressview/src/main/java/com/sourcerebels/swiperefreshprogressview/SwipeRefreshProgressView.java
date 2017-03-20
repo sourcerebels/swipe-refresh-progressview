235f6124acd8e52e0c816ba6d1b6fb44c9184279
Edu Rodríguez Castillo <edu@sourcerebels.com>
2017-03-20 12:55:25 +0100

    Error TextView TextAppearance

--- swiperefreshprogressview/src/main/java/com/sourcerebels/swiperefreshprogressview/SwipeRefreshProgressView.java
+++ swiperefreshprogressview/src/main/java/com/sourcerebels/swiperefreshprogressview/SwipeRefreshProgressView.java
@@ -1,6 +1,7 @@
 package com.sourcerebels.swiperefreshprogressview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
@@ -17,12 +18,12 @@ public class SwipeRefreshProgressView extends RelativeLayout {
 
     public SwipeRefreshProgressView(Context context) {
         super(context);
-        initialize(context);
+        initialize(context, null);
     }
 
     public SwipeRefreshProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initialize(context);
+        initialize(context, attrs);
     }
 
     public void hide() {
@@ -53,11 +54,34 @@ public class SwipeRefreshProgressView extends RelativeLayout {
         setVisibility(VISIBLE);
     }
 
-    private void initialize(Context context) {
+    private void initialize(Context context, AttributeSet attrs) {
         inflate(context, R.layout.view_swipe_refresh_progress, this);
         progressBar = (ProgressBar) findViewById(R.id.srp_progress_bar);
         swipeRefreshError = (SwipeRefreshLayout) findViewById(R.id.srp_swipe_refresh_error);
         errorMessage = (TextView) findViewById(R.id.srp_error_message);
+
+        if (attrs == null) {
+            return;
+        }
+
+        final int[] styleable = new int[]{
+                android.R.attr.textAppearance
+        };
+
+        TypedArray a = context.obtainStyledAttributes(attrs, styleable);
+        try {
+            for (int attrIndex = 0; attrIndex < styleable.length; attrIndex++) {
+                int attribute = styleable[attrIndex];
+                switch (attribute) {
+                    case android.R.attr.textAppearance:
+                        errorMessage.setTextAppearance(context, a.getResourceId(attrIndex, 0));
+                        break;
+                }
+
+            }
+        } finally {
+            a.recycle();
+        }
     }
 }
 
